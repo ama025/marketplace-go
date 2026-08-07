36b97ab28e54fb81fd5a677d68b2f5f95c21d0a6
A M <ananasbanan432@gmail.com>
2026-08-08 00:05:22 +0300

    feat(catalog): добавить чтение .env, подключение к PostgreSQL и регистрацию роутов

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -1,19 +1,72 @@
-package main 
+package main
 
 import (
-
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
+
+	"marketplace/internal/catalog/api"
+	"marketplace/internal/catalog/api/handlers"
+	"marketplace/internal/catalog/application/queries"
+	"marketplace/internal/catalog/infrastructure/persistence"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
+// main — точка входа в программу. Здесь инициализируется веб-сервер и маршруты.
 func main() {
-	r := gin.Default() //это функция которая создает новый экземпляр Gin для работы с маршрутами и запросами
 
-	r.GET("/health", func(ctx *gin.Context) { //этот маршрут возвращает JSON с статусом "ok" при GET запросе на "/health"
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatal(err)
+	}
+
+	appPort := os.Getenv("CATALOG_APP_PORT")
+	pgHost := os.Getenv("CATALOG_PG_HOST")
+	pgPort := os.Getenv("CATALOG_PG_PORT")
+	pgDB := os.Getenv("CATALOG_PG_DATABASE")
+	pgUser := os.Getenv("CATALOG_PG_USER")
+	pgPass := os.Getenv("CATALOG_PG_PASSWORD")
+	pgSSL := os.Getenv("CATALOG_PG_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		pgHost,
+		pgPort,
+		pgUser,
+		pgPass,
+		pgDB,
+		pgSSL,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("postgres ping error:", err)
+	}
+
+	brandRepo := persistence.NewBrandRepository(db)
+	listBrandsHandler := queries.NewBrandsHandler(brandRepo)
+	brandsHandler := handlers.NewBrandsHandler(listBrandsHandler)
+
+	r := gin.Default()
+
+	r.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"status": "ok"})
 	})
 
-	if err := r.Run(":9001"); err != nil { //этот код запускает сервер Gin на порту 9001 и ловит ошибки, если они возникают
+	api.RegisterRoutes(r, brandsHandler)
+
+	if err := r.Run(":" + appPort); err != nil {
 		log.Fatal(err)
 	}
 }
